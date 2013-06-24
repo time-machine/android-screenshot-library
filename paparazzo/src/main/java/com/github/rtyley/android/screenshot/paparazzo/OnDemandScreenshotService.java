05521d5ed00be6bcbb3081be5aa117846046276d
kumabotz <kumabotz@gmail.com>
2013-06-24 17:18:58 +0800

    Add start

--- paparazzo/src/main/java/com/github/rtyley/android/screenshot/paparazzo/OnDemandScreenshotService.java
+++ paparazzo/src/main/java/com/github/rtyley/android/screenshot/paparazzo/OnDemandScreenshotService.java
@@ -1,15 +1,17 @@
 package com.github.rtyley.android.screenshot.paparazzo;
 
 //
+import static java.lang.Thread.sleep;
 import static java.util.Arrays.asList;
 
 import com.android.ddmlib.IDevice;
 import com.android.ddmlib.MultiLineReceiver;
-//
+import com.android.ddmlib.RawImage;
 import com.github.rtyley.android.screenshot.paparazzo.processors.ScreenshotProcessor;
 //
+import java.util.HashMap;
 import java.util.List;
-//
+import java.util.Map;
 
 public class OnDemandScreenshotService {
   private final IDevice device;
@@ -22,9 +24,41 @@ public class OnDemandScreenshotService {
     this.logListenerCommandShell = new ScreenshotRequestListener();
   }
 
-  // todo: -
+  /**
+   * Start receiving and acting on paparazzo requests from the device.
+   */
+  public void start() {
+    Thread thread = new Thread(new LogCatCommandExecutor());
+    thread.setName(getClass().getSimpleName() + " logcat for " +
+        device.getSerialNumber());
+    thread.start();
+
+    try {
+      sleep(500); // ignore old output that logcat feeds us
+    } catch (InterruptedException e) {}
+
+    logListenerCommandShell.activate();
+  }
 
   //
+
+  private class LogCatCommandExecutor implements Runnable {
+    /**
+     * Reads only the 'main' log buffer, specifically to exclude the 'system'
+     * log - the system log can be extremely slow to cat on some devices, and
+     * it doesn't even have the 'screenshot_request' tag we're interested in.
+     */
+    private final static String LOGCAT_COMMAND =
+        "logcat -b main screenshot_request:D *:S";
+
+    @Override
+    public void run() {
+      try {
+        device.executeShellCommand(LOGCAT_COMMAND, logListenerCommandShell, 0);
+      } catch (Exception e) {}
+    }
+  }
+
   private class ScreenshotRequestListener extends MultiLineReceiver {
     private boolean activated = false, cancelled = false;
 
@@ -55,6 +89,31 @@ public class OnDemandScreenshotService {
   }
 
   private void takeScreenshotFor(String logLine) {
-    //
+    RawImage rawImage;
+    try {
+      rawImage = device.getScreenshot();
+    } catch (Exception e) {
+      return;
+    }
+
+    if (rawImage == null) {
+      return;
+    }
+
+    Map<String, String> keyValueMap = keyValueMapFor(logLine);
+    // todo: -
+  }
+
+  // expect log line to have kk=vv,kk=vv
+  private Map<String, String> keyValueMapFor(String logLine) {
+    Map<String, String> keyValueMap = new HashMap<String, String>();
+    for (String keyValuePair : logLine.split(",")) {
+      int separatorIndex = keyValuePair.indexOf("=");
+      if (separatorIndex > 0) {
+        keyValueMap.put(keyValuePair.substring(0, separatorIndex),
+            keyValuePair.substring(separatorIndex));
+      }
+    }
+    return keyValueMap;
   }
 }
