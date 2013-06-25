bde3b800530266506607d85e61dd80fab23a8c92
kumabotz <kumabotz@gmail.com>
2013-06-25 16:02:02 +0800

    Only request raw long lines for the moment

--- paparazzo/src/main/java/com/github/rtyley/android/screenshot/paparazzo/OnDemandScreenshotService.java
+++ paparazzo/src/main/java/com/github/rtyley/android/screenshot/paparazzo/OnDemandScreenshotService.java
@@ -60,7 +60,7 @@ public class OnDemandScreenshotService {
      * it doesn't even have the 'screenshot_request' tag we're interested in.
      */
     private final static String LOGCAT_COMMAND =
-        "logcat -b main screenshot_request:D *:S";
+        "logcat -v raw -b main screenshot_request:D *:S";
 
     @Override
     public void run() {
